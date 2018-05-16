93d3460a53bcfa20633227f5167a17f4cbed8176
signarapp <39178844+signarapp@users.noreply.github.com>
2018-05-16 01:10:49 +0100

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+// JavaScript from https://codepen.io/taniarascia/pen/dYvvYv
+
+(function ($) { // Begin jQuery
+  $(function() { // DOM ready
+    
+
+      
+      // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    
+      
+      // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+      
+  }); // end DOM ready
+    
+})(jQuery); // end jQuery
